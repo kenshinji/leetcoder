1a57fa40e728a9f424d5835b1372cefa4e29c56f
Fred Wang <fred.wang@klar.mx>
2022-09-15 20:21:24 +0200

    Clean code.

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const schedule = require('node-schedule');
 const { token, channelId } = require('./config.json');
 const LEETCODE_URL = 'https://leetcode.com';
 const LEETCODE_DATA = {"query":"query questionOfToday {\n\tactiveDailyCodingChallengeQuestion {\n\t\tdate\n\t\tuserStatus\n\t\tlink\n\t\tquestion {\n\t\t\tacRate\n\t\t\tdifficulty\n\t\t\tfreqBar\n\t\t\tfrontendQuestionId: questionFrontendId\n\t\t\tisFavor\n\t\t\tpaidOnly: isPaidOnly\n\t\t\tstatus\n\t\t\ttitle\n\t\t\ttitleSlug\n\t\t\thasVideoSolution\n\t\t\thasSolution\n\t\t\ttopicTags {\n\t\t\t\tname\n\t\t\t\tid\n\t\t\t\tslug\n\t\t\t}\n\t\t}\n\t}\n}\n","operationName":"questionOfToday"}
-// daily URL
-// https://jerrynsh.com/how-i-sync-daily-leetcoding-challenge-to-todoist/
+
+// Run everyday at 10:00 AM
+const CRON_STR = '* 10 * * *'
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -15,8 +16,8 @@ client.login(token);
 client.once('ready', () => {
 	console.log('I am ready!');
 
-    // run dailyChallenge() every day at 8:00 AM
-    const job = schedule.scheduleJob('27 * * * *', async function(){
+    // run daily job
+    const job = schedule.scheduleJob(CRON_STR, async function(){
         const response = await axios.post(LEETCODE_URL + '/graphql', LEETCODE_DATA);
         const msg = 'Today\'s Leetcode Daily Challenge: ' + LEETCODE_URL + response.data.data.activeDailyCodingChallengeQuestion.link;
         client.channels.cache.get(channelId).send(msg);
